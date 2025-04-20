8ba5de9ed12bb54e0c75ef90e79e0085f554f15a
tanishqsrivastavaa <tanishq280205@gmail.com>
2025-04-20 22:23:38 +0530

    Validation Error solved

--- src/config.py
+++ src/config.py
@@ -1,16 +1,20 @@
 from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
+import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
-# Load environment variables from .env file
 load_dotenv(dotenv_path=".env", override=True)
 
 
 class Settings(BaseSettings):
-    GROQ_API_KEY: str
-    GEMINI_API_KEY: str
-    WHISPER_URL: str
-    SANITIZE_URL: str
+    GROQ_API_KEY: str 
+    GEMINI_API_KEY: str 
+    WHISPER_URL: str  
+    SANITIZE_URL: str 
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
-
+    
+    
+    #If you have validation errors, uncomment the line below and comment the 'Config' Class:
+    # model_config = SettingsConfigDict(env_file=os.path.join(os.path.dirname(__file__), "..", ".env")) 
